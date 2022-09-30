9441d8fd8272d6feaaa70b709169a926d590aaf6
Stanislaw <98832573+cxrsedhappy@users.noreply.github.com>
2022-10-01 00:01:50 +0300

    Reconstructed task1

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -1,52 +1,60 @@
 package task1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         subtask1();
-
-        int num = subtask5(6); // 120
-        System.out.println(num);
+        subtask2();
+        subtask3(args);
+        System.out.println("[4] -------------------");
+        subtask4(1);
+        System.out.println();
+        System.out.println("Factorial of 5 is " + subtask5(5));
     }
     public static void subtask1(){
-        int[] myArray = new int[]{3, 5, 6, 23, 12, 65, 33, 69, 322, 228};
-
-        // Part with "for"
-        int sumOfArrayNum = 0;
-        for (int i : myArray) {
-            sumOfArrayNum += i;
+        System.out.println("[1] -------------------");
+        int[] array = new int[]{3, 2, 6, 7, 5};
+        int sum = 0;
+        for (int i = 0; i < array.length; i++){
+            sum += array[i];
+            System.out.println("Sum at " + i + " iteration: " + sum);
         }
-        System.out.println("for: " + sumOfArrayNum);
-
-        // Part with "while"
-        sumOfArrayNum = 0;
-        int i = 0;
-        while (i < myArray.length) {
-            sumOfArrayNum += myArray[i];
-            i++;
-        }
-        System.out.println("while: " + sumOfArrayNum);
-
-        // Part with "do while"
-        sumOfArrayNum = 0;
-        i = 0;
-        do {
-            sumOfArrayNum += myArray[i];
-            i++;
-        } while (i < myArray.length);
-        System.out.println("do while: " + sumOfArrayNum);
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + (double)sum / array.length);
     }
     public static void subtask2(){
-        System.out.println("asd");
+        System.out.println("[2] -------------------");
+        Scanner scan = new Scanner(System.in);
+        int min = 99999999, max = 0, sum = 0, i = 0;
+        do {
+            int num = scan.nextInt();
+            if (num < min) { min = num; }
+            if (num > max) { max = num; }
+            sum += num;
+            i++;
+        } while (i < 5);
+        System.out.println("Sum: " + sum);
+        System.out.println("Max number: " + max);
+        System.out.println("Min Number: " + min);
+
     }
-    public static void subtask3(){
-        System.out.println("zxc");
+    public static void subtask3(String[] args){
+        System.out.println("[3] -------------------");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
     }
-    public static void subtask4(){
-        System.out.println("zxc");
+    public static void subtask4(int num){
+        if (num < 11){
+            System.out.print((double)1 / num + " ");
+            subtask4(num + 1);
+        }
     }
     public static int subtask5(int num){
+        System.out.println("[5] -------------------");
         int sum = 1;
-        for (int i = 1; i < num; i++){
+        for (int i = 1; i <= num; i++){
             sum *= i;
         }
         return sum;
